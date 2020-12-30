66c36598d7087404d01f1380f2fecdbc772eeae6
김석진 <7271kim@naver.com>
2020-12-30 17:52:20 +0900

    [2020.12.30] SNS 연습

---
+++ chapter7/models/index.js
@@ -0,0 +1,30 @@
+const Sequelize = require('sequelize');
+const User = require('./user');
+const Comment = require('./comment');
+
+const env = process.env.NODE_ENV || 'development';
+const config = require('../config/config')[env];
+const db = {};
+
+const sequelize = new Sequelize(config.database, config.username, config.password, config);
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+db.User = User;
+db.Comment = Comment;
+
+User.init(sequelize);
+Comment.init(sequelize);
+
+User.associate(db);
+Comment.associate(db);
+
+User.create({
+  name:'바부팅이',
+  age:24,
+  married:false,
+  comment:'zzzzㅋㅋㅋ'
+});
+
+module.exports = db;
